c3349bc9ef4f64805a62b1d1e8beb8c33b8e8867
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2024-03-31 20:42:53 -0300

    :lipstick: add colors const

---
+++ lib/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class AppColor {
+    AppColor._();
+    static const Color textColor = Color.fromRGBO(239, 239, 239, 1);
+    static const Color primaryColor = Color.fromRGBO(46, 56, 86, 1);
+    static const Color secondaryColor = Color.fromRGBO(118, 128, 157, 1);
+    static const Color tertiaryColor = Color.fromRGBO(10, 9, 45, 1);
+    static const Color backgroundColor = Color.fromRGBO(4, 3, 31, 1);
+    static const Color accentColor = Color.fromRGBO(177, 86, 231, 1);
+    static const Color secondAccentColor = Color.fromRGBO(239, 176, 15, 1);
+}
