65bf9e4eee4a0ff73f36b79bbc5907bfc672ac9e
Erick Sousa <16109127+ericksantos12@users.noreply.github.com>
2024-04-28 23:33:03 -0300

    ♻️ change display dialog to snackbar

--- lib/view/view_login.dart
+++ lib/view/view_login.dart
@@ -1,9 +1,12 @@
 import 'dart:convert';
 
 import 'package:ctracker/constants/colors.dart';
+import 'package:ctracker/utils/snack_bar.dart';
 import 'package:ctracker/view/view_home.dart';
+import 'package:ctracker/view/view_signin.dart';
 import 'package:ctracker/widget/form_text_field.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:ctracker/widget/oauth.dart';
 import 'package:http/http.dart' as http;
@@ -17,7 +20,6 @@ class ViewLogin extends StatelessWidget {
   final emailInputController = TextEditingController();
   final passwordInputController = TextEditingController();
 
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   void displayDialog(context, title, text) => showDialog(
         context: context,
@@ -33,10 +35,11 @@ class ViewLogin extends StatelessWidget {
           "email": email,
           "password": password,
         }));
-      
+    
+
     if (kDebugMode) print(res.statusCode);
+
     if (res.statusCode == 200) {
-      if (kDebugMode) print(res.body);
       return res.body;
     }
     throw Exception('Email ou Senha incorretos');
@@ -47,152 +50,152 @@ class ViewLogin extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              const SizedBox(
-                height: 50,
-              ),
-              const Text(
-                "Logar-se no\n Champions Tracker\n",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
-                    color: AppColor.textColor),
-              ),
-              const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    OAuth(
-                      imagePath:
-                          'assets/images/discordlogo.png', //OAuth Discord
-                      borderColor: Color.fromRGBO(88, 101, 242, 1),
-                      containerColor: Color.fromRGBO(88, 101, 242, 1),
-                    ),
-                    SizedBox(width: 35),
-                    OAuth(
-                      imagePath:
-                          'assets/images/twitterlogo.png', //OAuth Twitter
-                      borderColor: Color.fromRGBO(29, 161, 242, 1),
-                      containerColor: Color.fromRGBO(29, 161, 242, 20),
-                    ),
-                    SizedBox(width: 35),
-                    OAuth(
-                      imagePath: 'assets/images/googlelogo.png', //OAuth Google
-                      borderColor: Colors.white,
-                      containerColor: Colors.white,
-                    ),
-                  ]),
-              const SizedBox(height: 25),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                        margin: const EdgeInsets.only(right: 25.0),
-                        child: const Divider(
-                          color: AppColor.secondaryColor,
-                          height: 36,
-                        )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(
+              height: 50,
+            ),
+            const Text(
+              "Logar-se no\n Champions Tracker\n",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                  color: AppColor.textColor),
+            ),
+            const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  OAuth(
+                    imagePath: 'assets/images/discordlogo.png', //OAuth Discord
+                    borderColor: Color.fromRGBO(88, 101, 242, 1),
+                    containerColor: Color.fromRGBO(88, 101, 242, 1),
                   ),
-                  const Text("ou", style: TextStyle(color: AppColor.textColor)),
-                  Expanded(
-                    child: Container(
-                        margin: const EdgeInsets.only(left: 25.0),
-                        child: const Divider(
-                          color: AppColor.secondaryColor,
-                          height: 36,
-                        )),
+                  SizedBox(width: 35),
+                  OAuth(
+                    imagePath: 'assets/images/twitterlogo.png', //OAuth Twitter
+                    borderColor: Color.fromRGBO(29, 161, 242, 1),
+                    containerColor: Color.fromRGBO(29, 161, 242, 20),
                   ),
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              FormTextField(
-                keyboardType: TextInputType.emailAddress,
-                labelText: "Email",
-                controller: emailInputController,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              FormTextField(
-                keyboardType: TextInputType.text,
-                labelText: "Senha",
-                controller: passwordInputController,
-                passwordField: true,
-              ),
-              const Center(
-                  child: Text('Esqueceu sua senha?',
-                      style: TextStyle(
-                          fontSize: 11, color: AppColor.accentColor))),
-              const SizedBox(
-                height: 40,
-              ),
-              ElevatedButton(
-                  onPressed: () async {
-                    var email = emailInputController.text;
-                    var password = passwordInputController.text;
+                  SizedBox(width: 35),
+                  OAuth(
+                    imagePath: 'assets/images/googlelogo.png', //OAuth Google
+                    borderColor: Colors.white,
+                    containerColor: Colors.white,
+                  ),
+                ]),
+            const SizedBox(height: 25),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                      margin: const EdgeInsets.only(right: 25.0),
+                      child: const Divider(
+                        color: AppColor.secondaryColor,
+                        height: 36,
+                      )),
+                ),
+                const Text("ou", style: TextStyle(color: AppColor.textColor)),
+                Expanded(
+                  child: Container(
+                      margin: const EdgeInsets.only(left: 25.0),
+                      child: const Divider(
+                        color: AppColor.secondaryColor,
+                        height: 36,
+                      )),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            FormTextField(
+              keyboardType: TextInputType.emailAddress,
+              labelText: "Email",
+              controller: emailInputController,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            FormTextField(
+              keyboardType: TextInputType.text,
+              labelText: "Senha",
+              controller: passwordInputController,
+              passwordField: true,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Center(
+                child: Text('Esqueceu sua senha?',
+                    style:
+                        TextStyle(fontSize: 11, color: AppColor.accentColor))),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  var email = emailInputController.text;
+                  var password = passwordInputController.text;
 
-                    try {
-                      var jwt = await attemptLogIn(email, password);
+                  try {
+                    var jwt = await attemptLogIn(email, password);
 
-                      storage.write(key: "jwt", value: jwt);
+                    storage.write(key: "jwt", value: jwt);
 
-                      if (kDebugMode) print("JWT: $jwt");
+                    if (kDebugMode) print("JWT: $jwt");
 
-                      Navigator.push(
+                    Navigator.push(
                         context,
                         MaterialPageRoute(
-                            builder: (context) => ViewHome.fromBase64(jwt)
-                        )
-                      );
-                    } catch (error) {
-                      displayDialog(context, "Um Erro Ocorreu", "Email ou Senha incorretos");
-                    }
-                  },
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: AppColor.primaryColor,
-                      foregroundColor: AppColor.textColor,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      padding: const EdgeInsets.only(
-                          left: 70, right: 70, top: 20, bottom: 20)),
-                  child: const Text("LOGAR")),
-              const SizedBox(
-                height: 20,
+                            builder: (context) => ViewHome.fromBase64(jwt)));
+                  } catch (error) {
+                    snackBar(context, "Email ou Senha incorretos");
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColor.primaryColor,
+                    foregroundColor: AppColor.textColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    padding: const EdgeInsets.only(
+                        left: 70, right: 70, top: 20, bottom: 20)),
+                child: const Text("LOGAR")),
+            const SizedBox(
+              height: 20,
+            ),
+            Center(
+                child: RichText(
+                    text: TextSpan(children: [
+              const TextSpan(
+                text: 'Não tem uma conta?',
+                style: TextStyle(
+                  fontSize: 11,
+                  color: AppColor.secondaryColor,
+                ),
               ),
-              Center(
-                  child: RichText(
-                      text: const TextSpan(children: [
-                TextSpan(
-                  text: 'Não tem uma conta?',
-                  style: TextStyle(
-                    fontSize: 11,
-                    color: AppColor.secondaryColor,
-                  ),
+              TextSpan(
+                text: ' Criar agora',
+                recognizer: TapGestureRecognizer()
+                  ..onTap = () async {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ViewSignin()));
+                  },
+                // o código acima não vai funcionar a não ser que o "const" do textspan seja removido (nao sei pq)
+                style: const TextStyle(
+                  fontSize: 11,
+                  color: AppColor.accentColor,
+                  decoration: TextDecoration.underline,
                 ),
-                TextSpan(
-                  text: ' Criar agora',
-                  // recognizer: TapGestureRecognizer()..onTap = () async {
-                  // Navigator.pushNamed(context, '/signin');
-                  // },
-                  // o código acima não vai funcionar a não ser que o "const" do textspan seja removido (nao sei pq)
-                  style: TextStyle(
-                    fontSize: 11,
-                    color: AppColor.accentColor,
-                    decoration: TextDecoration.underline,
-                  ),
-                ),
-              ]))),
-            ],
-          ),
+              ),
+            ]))),
+          ],
         ),
       ),
     );
