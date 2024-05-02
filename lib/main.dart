fcaa7f06fbb006ab076d7e7a408de335f7dd1336
Erick Sousa <16109127+ericksantos12@users.noreply.github.com>
2024-05-02 09:46:42 -0300

    ♻️ fix loading and separate verifyJwt

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
-import 'dart:convert';
-
+import 'package:ctracker/constants/colors.dart';
+import 'package:ctracker/utils/verify_jwt.dart';
 import 'package:ctracker/view/view_home.dart';
 import 'package:ctracker/view/view_login.dart';
-import 'package:ctracker/view/view_signup.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:ctracker/constants/colors.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 void main() async {
   runApp(MaterialApp(
@@ -21,18 +17,12 @@ void main() async {
 class Main extends StatelessWidget {
   const Main({super.key});
 
-Future<String> get verifyJwt async {
-  final storage = FlutterSecureStorage();
-  var jwt = await storage.read(key: "jwt");
-  if (jwt == null) return "";
-  return jwt;
-}
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: verifyJwt,
+        future: verifyJwt(),
         builder: (context, snapshot) {
-          if (!snapshot.hasData) return const CircularProgressIndicator();
+          if (!snapshot.hasData) return const Center(child: CircularProgressIndicator(color: Colors.white));
           if (snapshot.data != "") {
             var str = snapshot.data;
             var jwt = str!.split(".");
@@ -40,9 +30,6 @@ Future<String> get verifyJwt async {
             if (jwt.length != 3) {
               return ViewLogin();
             } else {
-              var payload = json.decode(ascii.decode(base64.decode(base64.normalize(jwt[1]))));
-              if (kDebugMode) print("Payload: $payload");
-
               return const ViewHome();
             }
           } else {
