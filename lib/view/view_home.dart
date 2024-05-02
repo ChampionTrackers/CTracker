99a66fc7c8eb3bfb52ae69627823c84e2e6a8dea
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2024-05-01 21:35:18 -0300

    :sparkles: implement navigation components

--- lib/view/view_home.dart
+++ lib/view/view_home.dart
@@ -1,30 +1,39 @@
 import 'dart:convert';
-
 import 'package:ctracker/constants/colors.dart';
+import 'package:ctracker/widget/bottom_navigation.dart';
+import 'package:ctracker/widget/tracker_appbar.dart';
+import 'package:ctracker/widget/tracker_drawer.dart';
 import 'package:flutter/material.dart';
 
-
 class ViewHome extends StatelessWidget {
-  const ViewHome(this.jwt, this.payload, {super.key});
+  // const ViewHome(this.jwt, this.payload, {super.key});
 
-  factory ViewHome.fromBase64(String jwt) => 
-    ViewHome(
-      jwt,
-      json.decode(
-        ascii.decode(
-          base64.decode(base64.normalize(jwt.split(".")[1]))
-        )
-      )
-    );
+  // factory ViewHome.fromBase64(String jwt) =>
+  //   ViewHome(
+  //     jwt,
+  //     json.decode(
+  //       ascii.decode(
+  //         base64.decode(base64.normalize(jwt.split(".")[1]))
+  //       )
+  //     )
+  //   );
 
-    final String jwt;
-    final Map<String, dynamic> payload;
+  //   final String jwt;
+  //   final Map<String, dynamic> payload;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: null,
-      backgroundColor: AppColor.backgroundColor,
+      appBar: const TrackerAppBar(),
+      endDrawer: const TrackerDrawer(),
+      bottomNavigationBar: const BottomNavigation(),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
+          backgroundColor: AppColor.secondAccentColor,
+          child: const Icon(Icons.search)),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -48,7 +57,9 @@ class ViewHome extends StatelessWidget {
                     },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: AppColor.secondAccentColor,
-                      textStyle: const TextStyle(color: AppColor.textColor, fontWeight:FontWeight.bold),
+                      textStyle: const TextStyle(
+                          color: AppColor.textColor,
+                          fontWeight: FontWeight.bold),
                     ),
                     child: const Text('Ver mais'),
                   ),
@@ -85,82 +96,90 @@ class ViewHome extends StatelessWidget {
   }
 
   Widget _buildCard(
-  String imagePath,
-  String title,
-) {
-  return Padding(
-    padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0), // Adicionando margem apenas na parte superior
-    child: Card(
-      margin: const EdgeInsets.fromLTRB(0, 0, 10, 0),
-      color: const Color.fromRGBO(46, 56, 86, 1),
-      child: SizedBox(
-        width: 220,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(top: 10),
-              height: 100,
-              width: double.infinity,
-              child: Image.asset(
-                imagePath,
-                fit: BoxFit.contain,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Center(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      title,
-                      style: const TextStyle(color: AppColor.textColor, fontSize: 18, fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(height: 10),
-                    const Row(
-                      children: [
-                        Icon(Icons.gamepad, color: AppColor.textColor),
-                        SizedBox(width: 5),
-                        Text('CS:GO', style: TextStyle(color: AppColor.textColor)),
-                      ],
-                    ),
-                    const SizedBox(height: 5),
-                    const Row(
-                      children: [
-                        Icon(Icons.location_on, color: AppColor.textColor),
-                        SizedBox(width: 5),
-                        Text('Virtual', style: TextStyle(color: AppColor.textColor)),
-                      ],
-                    ),
-                    const SizedBox(height: 5),
-                    const Row(
-                      children: [
-                        Icon(Icons.calendar_today, color: AppColor.textColor),
-                        SizedBox(width: 5),
-                        Text('Data: 10/05/2024', style: TextStyle(color: AppColor.textColor)),
-                      ],
-                    ),
-                    const SizedBox(height: 5),
-                    const Row(
-                      children: [
-                        Icon(Icons.people, color: AppColor.textColor),
-                        SizedBox(width: 5),
-                        Text('Participantes: 10', style: TextStyle(color: AppColor.textColor)),
-                      ],
-                    ),
-                  ],
+    String imagePath,
+    String title,
+  ) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(
+          8.0, 8.0, 8.0, 0), // Adicionando margem apenas na parte superior
+      child: Card(
+        margin: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+        color: const Color.fromRGBO(46, 56, 86, 1),
+        child: SizedBox(
+          width: 220,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                height: 100,
+                width: double.infinity,
+                child: Image.asset(
+                  imagePath,
+                  fit: BoxFit.contain,
                 ),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        title,
+                        style: const TextStyle(
+                            color: AppColor.textColor,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 10),
+                      const Row(
+                        children: [
+                          Icon(Icons.gamepad, color: AppColor.textColor),
+                          SizedBox(width: 5),
+                          Text('CS:GO',
+                              style: TextStyle(color: AppColor.textColor)),
+                        ],
+                      ),
+                      const SizedBox(height: 5),
+                      const Row(
+                        children: [
+                          Icon(Icons.location_on, color: AppColor.textColor),
+                          SizedBox(width: 5),
+                          Text('Virtual',
+                              style: TextStyle(color: AppColor.textColor)),
+                        ],
+                      ),
+                      const SizedBox(height: 5),
+                      const Row(
+                        children: [
+                          Icon(Icons.calendar_today, color: AppColor.textColor),
+                          SizedBox(width: 5),
+                          Text('Data: 10/05/2024',
+                              style: TextStyle(color: AppColor.textColor)),
+                        ],
+                      ),
+                      const SizedBox(height: 5),
+                      const Row(
+                        children: [
+                          Icon(Icons.people, color: AppColor.textColor),
+                          SizedBox(width: 5),
+                          Text('Participantes: 10',
+                              style: TextStyle(color: AppColor.textColor)),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    ),
-  );
-}
+    );
   }
+}
 
 class MyTable extends StatelessWidget {
   const MyTable({super.key});
