fe7070b02c6f5386140966f5f94c1ec8cea76cf4
Erick Sousa <16109127+ericksantos12@users.noreply.github.com>
2024-05-20 17:10:29 -0300

    💄 add edit profile view fields

--- lib/view/view_edit_profile.dart
+++ lib/view/view_edit_profile.dart
@@ -1,5 +1,4 @@
 import 'package:ctracker/constants/colors.dart';
-import 'package:ctracker/widget/bottom_navigation.dart';
 import 'package:ctracker/widget/form_text_field.dart';
 import 'package:ctracker/widget/tracker_appbar.dart';
 import 'package:ctracker/widget/tracker_drawer.dart';
@@ -14,30 +13,28 @@ class ViewEditProfile extends StatefulWidget {
 
 class _ViewEditProfileState extends State<ViewEditProfile> {
   final nameInputController = TextEditingController();
+  final nicknameInputController = TextEditingController();
+  final emailInputController = TextEditingController();
+  final confirmUpdatePasswordInputController = TextEditingController();
+  final newPasswordInputController = TextEditingController();
+  final confirmNewPasswordInputController = TextEditingController();
+  final oldPasswordInputController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const TrackerAppBar(),
       endDrawer: const TrackerDrawer(),
-      bottomNavigationBar: const BottomNavigation(),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
-          backgroundColor: AppColor.secondAccentColor,
-          child: const Icon(Icons.search)),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       body: SingleChildScrollView(
         padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
         child: Column(
           children: <Widget>[
-            const Center(
-              child: CircleAvatar(
-                radius: 125,
-                backgroundImage:
-                    NetworkImage("https://picsum.photos/seed/evolution/400"),
-              ),
+            const Text(
+              "Editar Perfil",
+              style: TextStyle(
+                  color: AppColor.textColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
             ),
             const SizedBox(
               height: 20,
@@ -46,6 +43,102 @@ class _ViewEditProfileState extends State<ViewEditProfile> {
               labelText: "Nome",
               controller: nameInputController,
             ),
+            const SizedBox(
+              height: 20,
+            ),
+            FormTextField(
+              labelText: "Username",
+              controller: nicknameInputController,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            FormTextField(
+              labelText: "Email",
+              controller: emailInputController,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              "Digite sua senha para confirmar as alterações",
+              style: TextStyle(color: AppColor.textColor),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            FormTextField(
+              labelText: "Senha",
+              controller: confirmUpdatePasswordInputController,
+              passwordField: true,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(horizontal: 35),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  backgroundColor: AppColor.primaryColor,
+                ),
+                child: const Text(
+                  "CONFIRMAR",
+                  style: TextStyle(
+                      color: AppColor.textColor, fontWeight: FontWeight.w600),
+                )),
+            const Divider(
+              color: AppColor.secondaryColor,
+              height: 50,
+            ),
+            const Text(
+              "Trocar a Senha",
+              style: TextStyle(
+                  color: AppColor.textColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            FormTextField(
+              labelText: "Senha Atual",
+              controller: newPasswordInputController,
+              passwordField: true,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            FormTextField(
+              labelText: "Nova Senha",
+              controller: newPasswordInputController,
+              passwordField: true,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            FormTextField(
+              labelText: "Confirmar Nova Senha",
+              controller: confirmNewPasswordInputController,
+              passwordField: true,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(horizontal: 35),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  backgroundColor: AppColor.primaryColor,
+                ),
+                child: const Text(
+                  "TROCAR",
+                  style: TextStyle(
+                      color: AppColor.textColor, fontWeight: FontWeight.w600),
+                )),
           ],
         ),
       ),
