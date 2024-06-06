32388f5704a6c64e545967f972f9d968b10bf164
Erick Sousa <16109127+ericksantos12@users.noreply.github.com>
2024-06-06 13:37:43 -0300

    ✨ backend tournament info (#25)
    * ➕ add intl for DateFormat
    
    * ✨ fetch tournaments from api
    
    * ♻️ rename from Championship to Tournament
    
    * 🚨 fix format
    
    * 👽️ update tournament model
    
    * 💄 update tracker card style
    
    * ➕ adicionado shared preferences
    
    * ✨ add tournament backend
    
    * ✨ add welcome screen
    
    * ✨ add drawer backend
    
    * 👷 add build to release
    
    * 🚨 run dart linter

---
+++ lib/controller/controller_tournament.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+
+import 'package:ctracker/model/team_model.dart';
+import 'package:ctracker/model/tournament_model.dart';
+import 'package:http/http.dart' as http;
+
+class TournamentController {
+  Future<Tournament> fetchTournamentDetails(int id) async {
+    final uriChampionship =
+        Uri.https("ctracker-server.onrender.com", "/v1/championships/$id");
+
+    final response = await http.get(
+      uriChampionship,
+      headers: {
+        "Content-Type": "application/json",
+        'Accept': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return Tournament.fromJson(json.decode(response.body));
+    } else {
+      throw Exception("Failed to load championship");
+    }
+  }
+
+  Future<List<Team>> fetchTeams(int id) async {
+    final uriTeams = Uri.https(
+        "ctracker-server.onrender.com", "/v1/championships/$id/teams");
+
+    final response = await http.get(
+      uriTeams,
+      headers: {
+        "Content-Type": "application/json",
+        'Accept': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      List<Team> teams = [];
+      for (var team in json.decode(response.body)) {
+        teams.add(Team.fromJson(team));
+      }
+      return teams;
+    } else {
+      throw Exception("Failed to load teams");
+    }
+  }
+}
--- lib/widget/tracker_drawer.dart
+++ lib/widget/tracker_drawer.dart
@@ -1,40 +1,100 @@
+import 'dart:convert';
+
 import 'package:ctracker/constants/colors.dart';
+import 'package:ctracker/model/user_model.dart';
+import 'package:ctracker/utils/verify_jwt.dart';
 import 'package:ctracker/view/view_login.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
 
-class TrackerDrawer extends StatelessWidget {
+class TrackerDrawer extends StatefulWidget {
   const TrackerDrawer({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      backgroundColor: AppColor.tertiaryColor,
-      child: ListView(padding: EdgeInsets.zero, children: <Widget>[
-        const UserAccountsDrawerHeader(
-          accountEmail: Text("user@mail.com"),
-          accountName: Text("Seu zé"),
-          currentAccountPicture: CircleAvatar(
-            child: Text("SZ"),
-          ),
-        ),
-        const ListTile(
-            leading: Icon(Icons.info_outline, color: AppColor.textColor),
-            title:
-                Text("Sobre nós", style: TextStyle(color: AppColor.textColor))),
-        ListTile(
-            leading: const Icon(Icons.logout, color: AppColor.textColor),
-            title: const Text("Log out",
-                style: TextStyle(color: AppColor.textColor)),
-            onTap: () async {
-              FlutterSecureStorage storage = const FlutterSecureStorage();
-              await storage.deleteAll();
+  State<TrackerDrawer> createState() => _TrackerDrawerState();
+}
 
-              if (!context.mounted) return;
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (context) => const ViewLogin()));
-            }),
-      ]),
-    );
+class _TrackerDrawerState extends State<TrackerDrawer> {
+  final storage = const FlutterSecureStorage();
+
+  Future<User> _fetchProfile() async {
+    var jwt = await verifyJwt();
+
+    // Fetch profile data from API
+    var res = await http.get(
+        Uri.parse("https://ctracker-server.onrender.com/v1/profile"),
+        headers: {
+          "Content-Type": "application/json",
+          'Accept': 'application/json',
+          "Authorization": "Bearer $jwt"
+        });
+
+    if (res.statusCode == 200) {
+      if (kDebugMode) print("Response: ${res.body}");
+      return User.fromJson(json.decode(res.body) as Map<String, dynamic>);
+    } else {
+      throw Exception("Request failed with status: ${res.statusCode}");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _fetchProfile(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Drawer(
+              backgroundColor: AppColor.tertiaryColor,
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          } else if (snapshot.hasError) {
+            return const Drawer(
+              backgroundColor: AppColor.tertiaryColor,
+              child: Center(
+                child: Text("Erro ao carregar perfil"),
+              ),
+            );
+          } else {
+            return Drawer(
+              backgroundColor: AppColor.tertiaryColor,
+              child: ListView(padding: EdgeInsets.zero, children: <Widget>[
+                UserAccountsDrawerHeader(
+                  accountEmail:
+                      Text(snapshot.data?.email ?? "seu.ze@gmail.com"),
+                  accountName: Text(snapshot.data?.name ?? "Seu zé"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage(snapshot.data?.picture ??
+                        "https://api.dicebear.com/8.x/lorelei/png?seed=seu.ze"),
+                  ),
+                ),
+                const ListTile(
+                    leading:
+                        Icon(Icons.info_outline, color: AppColor.textColor),
+                    title: Text("Sobre nós",
+                        style: TextStyle(color: AppColor.textColor))),
+                ListTile(
+                    leading:
+                        const Icon(Icons.logout, color: AppColor.textColor),
+                    title: const Text("Log out",
+                        style: TextStyle(color: AppColor.textColor)),
+                    onTap: () async {
+                      FlutterSecureStorage storage =
+                          const FlutterSecureStorage();
+                      await storage.deleteAll();
+
+                      if (!context.mounted) return;
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const ViewLogin()));
+                    }),
+              ]),
+            );
+          }
+        });
   }
 }
