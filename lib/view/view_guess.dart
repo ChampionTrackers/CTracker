4af84b9e8a793185071deb230b3386bc98119209
Erick Sousa <16109127+ericksantos12@users.noreply.github.com>
2024-06-12 23:53:13 -0300

    💄 finish guess page

--- lib/view/view_guess.dart
+++ lib/view/view_guess.dart
@@ -1,269 +1,262 @@
+import 'package:ctracker/constants/colors.dart';
+import 'package:ctracker/widget/tracker_appbar.dart';
 import 'package:flutter/material.dart';
- 
+
 class ViewGuess extends StatefulWidget {
   const ViewGuess({super.key});
   @override
   State<ViewGuess> createState() => _ViewGuessState();
 }
- 
+
 class _ViewGuessState extends State<ViewGuess> {
- 
+  double _sliderValue = 0; // Valor inicial do slider
+  final _maxValue = 100;
+
   @override
   Widget build(BuildContext context) {
-                   
-  return Container(
-  color: const Color.fromRGBO(10, 9, 45, 1), 
-  child: SingleChildScrollView(
-    padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-    child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                 Container(
-                  margin: const EdgeInsets.only(top: 40, bottom: 20),
-                   child: const Center(
-                     child: Text(
-                        'Faça seu Palpite',
-                          style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                        ),
-                        textAlign: TextAlign.center, // Centraliza o texto horizontalmente
+    return Scaffold(
+      appBar: const TrackerAppBar(),
+      backgroundColor: AppColor.backgroundColor,
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const Center(
+              child: Text(
+                'Faça seu Palpite',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Center(
+              child: Text(
+                'Escolha a equipe ganhadora:',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    SizedBox(
+                      height: 50,
+                      child: Image.asset('./assets/graphics/baixo-t.png'),
+                    ),
+                    const SizedBox(height: 10),
+                    const CircleAvatar(
+                      backgroundColor: AppColor.secondaryColor,
+                      radius: 50,
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      '[time1]',
+                      style: TextStyle(
+                        color: AppColor.textColor,
+                        fontSize: 20,
                       ),
-                    ),                                
-                  ), 
-
-                  Container(
-                    margin: const EdgeInsets.only(top: 10, bottom: 20),                               
-                      child: const Center(
+                    ),
+                    const SizedBox(height: 20),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.add),
+                      color: AppColor.textColor,
+                      iconSize: 50,
+                    )
+                  ],
+                ),
+                Container(
+                  decoration: const BoxDecoration(
+                      color: AppColor.tertiaryColor,
+                      borderRadius: BorderRadius.all(Radius.circular(15))),
+                  margin: const EdgeInsets.only(top: 60, bottom: 0),
+                  padding: const EdgeInsets.fromLTRB(20, 5, 20, 15),
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
                         child: Text(
-                          'Escolha a equipe ganhadora:',
-                            style: TextStyle(
+                          'VS',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 32,
+                              fontWeight: FontWeight.w500),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Center(
+                        child: Text(
+                          '00:33:54',
+                          style: TextStyle(
                             color: Colors.white,
                             fontSize: 20,
-                            ),
-                            textAlign: TextAlign.center, 
                           ),
-                        ),                                
-                      ),            
-
-                              
-                ClipRect(                  
-                  child: Row(                    
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [                     
-                      Container(
-                        margin: const EdgeInsets.only(top: 60, bottom: 0),
-                        height: 100,
-                        width: 100,
-                        decoration: BoxDecoration(
-                          color: const Color.fromRGBO(46, 56, 86, 1),
-                          borderRadius: BorderRadius.circular(100),
+                          textAlign: TextAlign.center,
                         ),
                       ),
-
-                            Container(
-                                margin: const EdgeInsets.only(top: 60, bottom: 0),
-                                height: 70,
-                                width: 150,                                                  
-                                child: const Center(
-                                  child: Text(
-                                    'VS',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 24,
-                                    ),
-                                    textAlign: TextAlign.center, 
-                                  ),
-                                ),
-                              ),
-
-                                      Container(
-                                        margin: const EdgeInsets.only(top: 60, bottom: 0),
-                                        height: 100,
-                                        width: 100,
-                                        decoration: BoxDecoration(
-                                          color: const Color.fromRGBO(46, 56, 86, 1),
-                                          borderRadius: BorderRadius.circular(100),
-                                        ),
-                                      ),
-
+                      Center(
+                        child: Text(
+                          'Tempo restante',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 10,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
                     ],
                   ),
                 ),
-
-                ClipRect(                  
-                  child: Row(                    
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [                     
+                Column(
+                  children: [
+                    const SizedBox(height: 60),
+                    const CircleAvatar(
+                      backgroundColor: AppColor.secondaryColor,
+                      radius: 50,
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      '[time2]',
+                      style: TextStyle(
+                        color: AppColor.textColor,
+                        fontSize: 20,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.add),
+                      color: AppColor.textColor,
+                      iconSize: 50,
+                    )
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 40),
+            Container(
+              padding: const EdgeInsets.all(10),
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: AppColor.tertiaryColor,
+                borderRadius: BorderRadius.all(Radius.circular(15)),
+              ),
+              child: Column(
+                children: [
+                  SizedBox(
+                      height: 60,
+                      child: Image.asset('./assets/images/coin.png')),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  const Text(
+                    'Insira a quantidade de CTCoins',
+                    style: TextStyle(
+                        color: AppColor.textColor,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const Text(
+                        'Valor: ',
+                        style: TextStyle(
+                            color: AppColor.textColor,
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold),
+                      ),
                       Container(
-                        margin: const EdgeInsets.only(top: 0, bottom: 20),
-                        height: 100,
-                        width: 100,
-                        child: const Center(
+                        decoration: const BoxDecoration(
+                          color: AppColor.backgroundColor,
+                          borderRadius: BorderRadius.all(Radius.circular(5)),
+                        ),
+                        width: 50,
+                        alignment: Alignment.centerRight,
+                        child: Padding(
+                          padding: const EdgeInsets.all(2),
                           child: Text(
-                            '[time 1]',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              ),
-                            textAlign: TextAlign.center, 
+                            '\$${_sliderValue.toStringAsFixed(0)}',
+                            style: const TextStyle(
+                                color: AppColor.secondAccentColor,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14),
                           ),
                         ),
-                      ),
-                            
-                                      Container(
-                                        margin: const EdgeInsets.only(top: 0, bottom: 20),
-                                        height: 100,
-                                        width: 100,
-                                        child: const Center(
-                                          child: Text(
-                                            '[time 2]',
-                                            style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 20,
-                                            ),
-                                            textAlign: TextAlign.center, 
-                                          ),
-                                        ),
-                                      ),
+                      )
                     ],
                   ),
-                ),
-
-                 ClipRect(                  
-                  child: Row(                    
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [                     
-                      Container(
-                        margin: const EdgeInsets.only(top: 0, bottom: 20),
-                        height: 40,
-                        width: 100,
-                        child: const Center(
-                          child: Text(
-                            '+',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 40,
-                              ),
-                            textAlign: TextAlign.center, 
+                  Slider(
+                    value: _sliderValue,
+                    min: 0,
+                    max: _maxValue.toDouble(),
+                    divisions: 100,
+                    activeColor: AppColor.secondAccentColor,
+                    inactiveColor: AppColor.secondAccentColor.withOpacity(0.3),
+                    onChanged: (double value) {
+                      setState(() {
+                        _sliderValue = value;
+                      });
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          '0',
+                          style: TextStyle(
+                            color: AppColor.textColor,
+                            fontSize: 12,
                           ),
                         ),
-                      ),
-                            
-                                      Container(
-                                        margin: const EdgeInsets.only(top: 0, bottom: 20),
-                                        height: 40,
-                                        width: 100,
-                                        child: const Center(
-                                          child: Text(
-                                            '+',
-                                            style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 40,
-                                            ),
-                                            textAlign: TextAlign.center, 
-                                          ),
-                                        ),
-                                      ),
-                    ],
-                  ),
-                ),
-
-                ClipRect(
-  child: Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Expanded(
-        child: Container(
-          margin: const EdgeInsets.only(top:40, bottom: 20),
-          height: 250,
-          decoration: BoxDecoration(
-            color: const Color.fromRGBO(46, 56, 86, 1),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          // ignore: prefer_const_constructors
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: const Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Insira a quantidade de CTcoins',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                  ),
-                ),
-             /* const SizedBox(height: 20),               
-                PriceSlider(),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: secondAccentColor,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+                        Text(
+                          _maxValue.toString(),
+                          style: const TextStyle(
+                            color: AppColor.textColor,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  child: const Text('Adicionar'),
-                ),*/
-              ],
-            ),
-          ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          AppColor.primaryColor, // Cor de fundo do botão
+                      foregroundColor: Colors.white, // Cor do texto do botão
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(10.0), // Borda do botão
+                      ),
+                    ),
+                    child: const Text(
+                      'Confirmar Palpite',
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
-    ],
-  ),
-),
-
-
-/*
-             class PriceSlider extends StatefulWidget {
-              @override
-            _PriceSliderState createState() => _PriceSliderState();
-          }
-
-              class _PriceSliderState extends State<PriceSlider> {
-                double _currentPrice = 50;
-
-                @override
-                Widget build(BuildContext context) {
-                  return Column(
-                    children: [
-                      Text(
-                        'Preço: \$${_currentPrice.toStringAsFixed(2)}',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                        ),
-                      ),
-                      Slider(
-                        value: _currentPrice,
-                        min: 0,
-                        max: 100,
-                        divisions: 100,
-                        label: _currentPrice.toStringAsFixed(2),
-                        onChanged: (double value) {
-                          setState(() {
-                            _currentPrice = value;
-                          });
-                        },
-                      ),
-                    ],
-                  );
-                }
-              }
-*/
-               
-              ],
-            ),
-  ),
-);
-
-
-
+    );
+  }
 }
-}       
